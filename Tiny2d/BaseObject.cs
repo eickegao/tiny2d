52f94d581cb043b34002d9d75b1789da52cfd9d2
Gao <eickegao@gmail.com>
2013-05-08 18:43:41 -0400

    finished BaseObject class

--- Tiny2d/BaseObject.cs
+++ Tiny2d/BaseObject.cs
@@ -21,6 +21,7 @@ namespace Tiny2d
 		private int _tag;
 		private int _zOrder;
 
+		private bool _isTransformDirty;
 		private float _rotation;
 		private float _scaleX;
 		private float _scaleY;
@@ -62,198 +63,160 @@ namespace Tiny2d
 		{
 			get { return _zOrder;}
 		}
+			
+		public float Rotation 
+		{
+			get { return _rotation; }
+			set { if (_rotation != value) 
+					{
+						_rotation = value;
+						_isTransformDirty = true;
+					}
+				}
+		}
 		
-		private float _rotation;
-		private float _scaleX;
-		private float _scaleY;
-		private Point _position;
-		private bool _isRunning;
-		private bool _isEnable;
-		private Point _anchorPoint;
-		private int _width;
-		private int _height;
+		public float scaleX 
+		{
+			get { return _scaleX; }
+			set {
+					_scaleX = value;
+					_isTransformDirty = true;
+				}
+		}
 		
-		private List<BaseObject> _children;
-		private bool _isVisible;
-		private BaseObject _parent;
-		private object _userData;
-		public IList<BaseObject> Children {
+		public float scaleY 
+		{
+			get { return _scaleY; }
+			set {
+					_scaleY = value;
+					_isTransformDirty = true;
+				}
+		}
+
+		public  Point Position
+		{
+			get { return _position;}
+			set { _position = value;}
+		}
+
+		public bool isRunning
+		{
+			get { return _isRunning;}
+			set { _isRunning = value;}
+		}
+
+		public bool isEnable
+		{
+			get { return _isEnable;}
+			set { _isEnable = value;}
+		}
+
+		public  Point anchorPoint
+		{
+			get { return _anchorPoint;}
+			set { _anchorPoint = value;}
+		}
+
+		public int Width
+		{
+			get { return _width;}
+			set { _width = value;}
+		}
+
+		public int Height
+		{
+			get { return _height;}
+			set { _height = value;}
+		}
+
+		public bool isVisible
+		{
+			get { return _isVisible;}
+			set { _isVisible = value;}
+		}
+
+		public BaseObject Parent
+		{
+			get { return _parent;}
+			set { _parent = value;}
+		}
+
+		public object userData
+		{
+			get { return _userData;}
+			set { _userData = value;}
+		}
+
+
+		public List<BaseObject> Children {
 			get { return _children; }
 		}
 		#endregion
-	}
 
-	public class CocosNode {
-
-
-		public int Tag { get; set; }
-		public object UserData { get; set; }
-		
-		public virtual float Rotation {
-			get { return _rotation; }
-			set {
-				if (_rotation != value) {
-					_rotation = value;
-					_isTransformDirty = true;
-				}
-			}
-		}
-		
-		public float ScaleX {
-			get { return _scaleX; }
-			set {
-				_scaleX = value;
-				_isTransformDirty = true;
-			}
-		}
-		
-		public float ScaleY {
-			get { return _scaleY; }
-			set {
-				_scaleY = value;
-				_isTransformDirty = true;
-			}
-		}
-		
-		public float Scale {
-			get {
-				if (ScaleX == ScaleY) {
-					return ScaleX;
-				} else {
-					throw new InvalidOperationException("ScaleX and ScaleY do not match");
-				}
-			}
-			set {
-				ScaleX = ScaleY = value;
-				_isTransformDirty = true;
-			}
-		}
-		
-		public PointF TransformAnchor {
-			get { return _transformAnchor; }
-			set {
-				_transformAnchor = value;
-				_isTransformDirty = true;
-			}
-		}
-		
-		public PointF AnchorPoint {
-			get { return _anchorPoint; }
-			set {
-				if (_anchorPoint != value) {
-					_anchorPoint = value;
-					TransformAnchor = new PointF(ContentSize.Width * _anchorPoint.X, ContentSize.Height * _anchorPoint.Y);
-				}
-			}
-		}
-		
-		public virtual SizeF ContentSize {
-			get { return _contentSize; }
-			set {
-				if (_contentSize != value) {
-					_contentSize = value;
-					TransformAnchor = new PointF(_contentSize.Width * AnchorPoint.X, _contentSize.Height * AnchorPoint.Y);
-				}
-			}
-		}
-		
-		public bool IsRelativeAnchorPoint {
-			get { return _isRelativeAnchorPoint; }
-			set {
-				_isRelativeAnchorPoint = value;
-				_isTransformDirty = true;
-			}
-		}
-		
-		public CocosNode() {
-			IsRunning = false;
-			_rotation = 0;
-			ScaleX = ScaleY = 1;
-			TransformAnchor = PointF.Empty;
-			AnchorPoint = PointF.Empty;
-			ContentSize = SizeF.Empty;
-			IsRelativeAnchorPoint = true;
-			_isTransformDirty = true;
-			Grid = null;
-			Visible = true;
-			Tag = CocosNodeTagInvalid;
-			_zorder = 0;
-			_camera = null;
-			_children = new List<CocosNode>(4);
-			UserData = null;
-			_position = PointF.Empty;
-		}
-		
-		public void CleanUp() {
-			StopAllActions();
+		public BaseObject(){
+			_name = "";
+			_id = 0;
+			_tag = TAG_INVALID;
+			_zOrder = 0;
 			
-			foreach (CocosNode child in _children) {
+			_isTransformDirty = false;
+			_rotation = 0;
+			_scaleX = 1;
+			_scaleY = 1;
+			_position = Point.Zero;
+			_isRunning = false;
+			_isEnable = true;
+			_anchorPoint = Point.Zero;
+			_width = 0;
+			_height = 0;
+			_isVisible = true;
+
+			_children = new List<BaseObject>();
+			_parent = null;
+			_userData = null;
+		}
+
+		public void CleanUp() 
+		{
+			foreach (BaseObject child in _children) {
 				child.CleanUp();
 			}
 		}
-		
-		public CocosNode AddChild(CocosNode child) {
-			return AddChild(child, child.ZOrder, child.Tag);
-		}
-		
-		public CocosNode AddChild(CocosNode child, int zOrder) {
-			return AddChild(child, zOrder, child.Tag);
-		}
-		
-		public virtual CocosNode AddChild(CocosNode child, int z, int tag) {
-			if (child == null) {
+
+		public virtual BaseObject AddChild(BaseObject child) 
+		{
+			if (child == null) 
+			{
 				throw new ArgumentNullException("child");
 			}
-			if (child.Parent != null) {
+			if (child.Parent != null) 
+			{
 				throw new ArgumentException("Child already has a parent", "child");
 			}
-			
-			child.Tag = tag;
-			
-			InsertChild(child, z);
+
+			InsertChild(child);
+
 			child.Parent = this;
 			
-			if (IsRunning) {
+			if (_isRunning) 
+			{
 				child.OnEnter();
 			}
 			
 			return this;
 		}
-		
-		/// <summary>
-		/// Changing an object's position is done so often
-		/// it's probably cheaper to set it existing PointF
-		/// object than to constantly create new ones
-		/// </summary>
-		public void SetPosition(float x, float y) {
-			_position.X = x;
-			_position.Y = y;
-			
-			_isTransformDirty = true;
-		}
-		
-		public void SetPosition(PointF point) {
-			_position = point;
-			_isTransformDirty = true;
-		}
-		
-		public void MoveBy(float dx, float dy) {
-			_position.X = _position.X + dx;
-			_position.Y = _position.Y + dy;
-			
-			_isTransformDirty = true;
-		}
-		
-		public PointF Position {
-			get { return _position; }
-		}
-		
-		private void InsertChild(CocosNode child, int z) {
+
+		private void InsertChild(BaseObject child) 
+		{
 			int i = 0;
 			bool added = false;
-			
-			foreach (CocosNode node in _children) {
-				if (node.ZOrder > z) {
+
+			int z = child.ZOrder;
+
+			foreach (BaseObject node in _children)
+			{
+				if (node.ZOrder > z) 
+				{
 					added = true;
 					_children.Insert(i, child);
 					break;
@@ -261,29 +224,38 @@ namespace Tiny2d
 				++i;
 			}
 			
-			if (!added) {
+			if (!added) 
+			{
 				_children.Add(child);
 			}
 			
-			child._zorder = z;
+			child._zOrder = z;
 		}
-
 		
-		public virtual void RemoveChild(CocosNode child, bool cleanup) {
-			if (child != null) {
-				if (Children.Contains(child)) {
+		
+		public virtual void RemoveChild(BaseObject child, bool cleanup) 
+		{
+			if (child != null) 
+			{
+				if (Children.Contains(child)) 
+				{
 					DetachChild(child, cleanup);
 				}
 			}
 		}
 		
-		public void RemoveChildByTag(int tag, bool cleanup) {
+		public void RemoveChildByTag(int tag, bool cleanup) 
+		{
 			RemoveChild(GetChildByTag(tag), cleanup);
 		}
 		
-		public void RemoveAllChildren(bool cleanup) {
-			_children.Each(delegate(CocosNode child) {
-				if (IsRunning) {
+		public void RemoveAllChildren(bool cleanup) 
+		{
+
+			_children.ForEach(delegate(BaseObject child) 
+			               {
+				if(_isRunning) 
+				{
 					child.OnExit();
 				}
 				
@@ -297,12 +269,14 @@ namespace Tiny2d
 			_children.Clear();
 		}
 		
-		private void DetachChild(CocosNode child, bool cleanup) {
-			if (IsRunning) {
+		private void DetachChild(BaseObject child, bool cleanup) {
+			if (_isRunning) 
+			{
 				child.OnExit();
 			}
 			
-			if (cleanup) {
+			if (cleanup) 
+			{
 				child.CleanUp();
 			}
 			
@@ -311,180 +285,82 @@ namespace Tiny2d
 			_children.Remove(child);
 		}
 		
-		public CocosNode GetChildByTag(int tag) {
-			return _children.Where(child => child.Tag == tag).FirstOrDefault();
+		public BaseObject GetChildByTag(int tag) 
+		{
+			BaseObject result = null;
+			foreach (BaseObject node in _children)
+			{
+				if(node.Tag == tag)
+				{
+					result = node;
+					break;
+				}
+			}
+			return result;
 		}
 		
-		public virtual void Draw() {
+		public virtual void Draw()
+		{
+
 		}
 		
-		public virtual void Visit() {
-			if (!Visible) {
+		public virtual void Visit() 
+		{
+			if (!Visible) 
+			{
 				return;
 			}
 			
-			GL.PushMatrix();
-			
-			if (Grid != null && Grid.Active) {
-				Grid.BeforeDraw();
-				TransformAncestors();
-			}
-			
-			Transform();
-			
-			foreach (CocosNode child in _children) {
-				if (child.ZOrder < 0) {
+			foreach (BaseObject child in _children) 
+			{
+				if (child.ZOrder < 0)
+				{
 					child.Visit();
-				} else {
+				} 
+				else 
+				{
 					break;
 				}
 			}
 			
 			Draw();
 			
-			foreach (CocosNode child in _children) {
-				if (child.ZOrder >= 0) {
+			foreach (BaseObject child in _children) 
+			{
+				if (child.ZOrder >= 0) 
+				{
 					child.Visit();
 				}
 			}
-			
-			if (Grid != null && Grid.Active) {
-				Grid.AfterDraw(Camera);
-			}
-			
-			GL.PopMatrix();
 		}
 		
-		private void TransformAncestors() {
-			if (Parent != null) {
-				Parent.TransformAncestors();
-				Parent.Transform();
-			}
-		}
 		
-		public void Transform() {
-			if (!(Grid != null && Grid.Active)) {
-				Camera.Locate();
-			}
-			
-			if (IsRelativeAnchorPoint && (TransformAnchor.X != 0 || TransformAnchor.Y != 0)) {
-				GL.Translate(-TransformAnchor.X, -TransformAnchor.Y, VertexZ);
-			}
-			
-			if (TransformAnchor.X != 0 || TransformAnchor.Y != 0) {
-				GL.Translate(Position.X + TransformAnchor.X, Position.Y + TransformAnchor.Y, VertexZ);
-			} else if (Position.X != 0 || Position.Y != 0) {
-				GL.Translate(Position.X, Position.Y, VertexZ);
-			}
-			
-			if (Rotation != 0) {
-				GL.Rotate(-Rotation, 0, 0, 1);
-			}
-			
-			if (ScaleX != 1 || ScaleY != 1) {
-				GL.Scale(ScaleX, ScaleY, 1);
-			}
-			
-			if (TransformAnchor.X != 0 || TransformAnchor.Y != 0) {
-				GL.Translate(-TransformAnchor.X, -TransformAnchor.Y, VertexZ);
-			}
-		}
 		
-		public virtual void OnEnter() {
-			foreach (CocosNode child in _children) {
+		public virtual void OnEnter() 
+		{
+			foreach (BaseObject child in _children) 
+			{
 				child.OnEnter();
 			}
-			
-			//ActivateTimers();
-			
-			IsRunning = true;
+			_isRunning = true;
 		}
-		
-		public void OnEnterTransitionDidFinish() {
-			foreach (CocosNode child in _children) {
+
+		public void OnEnterTransitionDidFinish() 
+		{
+			foreach (BaseObject child in _children) 
+			{
 				child.OnEnterTransitionDidFinish();
 			}
 		}
 		
-		public virtual void OnExit() {
-			//DeactivateTimers();
-			
-			IsRunning = false;
-			
-			foreach (CocosNode child in _children) {
+		public virtual void OnExit() 
+		{
+			_isEnable = false;
+			foreach (BaseObject child in _children) 
+			{
 				child.OnExit();
 			}
 		}
-		
-		public virtual Action RunAction(Action action) {
-			if (action == null) {
-				throw new ArgumentNullException("action");
-			}
-			
-			ActionManager.Instance.AddAction(action, this);
-			
-			return action;
-		}
-		
-		public void StopAllActions() {
-			ActionManager.Instance.RemoveAllActionsForTarget(this);
-		}
-		
-		public void StopAction(Action action) {
-			ActionManager.Instance.RemoveAction(action);
-		}
-		
-		#region Transform
-		private CGAffineTransform NodeToParentTransform() {
-			if (_isTransformDirty) {
-				_transform = CGAffineTransform.MakeIdentity();
-				
-				if (!IsRelativeAnchorPoint) {
-					_transform.Translate(TransformAnchor.X, TransformAnchor.Y);
-				}
-				
-				_transform.Translate((int)Position.X, (int)Position.Y);
-				_transform.Rotate(Rotation.ToRadians());
-				_transform.Scale(ScaleX, ScaleY);
-				_transform.Translate(-TransformAnchor.X, -TransformAnchor.Y);
-				
-				_isTransformDirty = false;
-			}
-			
-			return _transform;
-		}
-		
-		private CGAffineTransform NodeToWorldTransform() {
-			CGAffineTransform t = NodeToParentTransform();
-			
-			for (CocosNode parent = Parent; parent != null; parent = parent.Parent) {
-				t = MTSub.CGAffineTransform.Concat(t, parent.NodeToParentTransform());
-			}
-			
-			return t;
-		}
-		
-		private CGAffineTransform WorldToNodeTransform() {
-			CGAffineTransform t = NodeToWorldTransform();
-			
-			return MTSub.CGAffineTransform.Invert(t);
-		}
-		
-		public PointF ConvertToNodeSpace(PointF worldPoint) {
-			return WorldToNodeTransform().TransformPoint(worldPoint);
-		}
-		
-		public PointF ConvertToWorldSpace(PointF nodePoint) {
-			return NodeToWorldTransform().TransformPoint(nodePoint);
-		}
-		
-		#endregion
-		
-		#region Timers
-		
-		
-		
-		
-		#endregion
 	}
+
 }
